6de0414a40501616c1e0bbc16cdb5e73a0918dc8
Allen <Allen@Mains-MacBook-Pro.local>
2019-01-12 10:10:15 -0500

    Added Spark support

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public class RobotMap {
+
+    public static final double clPIDl[] = {1.0, 0, 0}; // Placeholder
+    public static final double clPIDr[] = {1.0, 0, 0}; // Placeholder
+    public static final double videal = 1.5; // Placeholder
+
+    public static final int motors[] = {1, 2, 3, 4}; // In this order: FL, FR, BL, BR
+}
