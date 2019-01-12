e72744dfda313187e1ea315f2dba79c1ad17337f
ryan10145 <chang.ryan1@yahoo.com>
2019-01-12 11:30:38 -0500

    Add Basic Structure of Code

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+public class RobotMap {
+
+    /**
+     * Contains constants for use without the entire robot
+     * All constants should be ordered by subsystem/use
+     * The units of each measurement should be specified in a comment
+     */
+
+    
+}
---
+++ src/main/java/frc/util/SnailController.java
@@ -0,0 +1,14 @@
+package frc.util;
+
+import edu.wpi.first.wpilibj.XboxController;
+
+public class SnailController extends XboxController {
+
+    /** 
+     * Xbox controller optimized for our drive team.
+     */
+
+	public SnailController(int port) {
+		super(port);
+	}
+}
