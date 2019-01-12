7a17b0a2be15c168df0b50ab213b5a0cb9de66f4
ryan10145 <chang.ryan1@yahoo.com>
2019-01-12 14:26:19 -0500

    Add Hatch Intake File

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# 2019-Robot
+# Hatch Intake
 
-[![Build Status](https://travis-ci.com/FRC1257/2019-Robot.svg?branch=master)](https://travis-ci.com/FRC1257/2019-Robot)
+[![Build Status](https://travis-ci.com/FRC1257/2019-Robot.svg?branch=HatchIntake)](https://travis-ci.com/FRC1257/2019-Robot)
 
-Code for our 2019 Season Robot (Name TBD).
+Code for our Hatch Intake
 
 ## WPILib VendorLib Notes
 
---
+++ src/main/java/frc/subsystems/HatchIntake.java
@@ -0,0 +1,26 @@
+package frc.subsystems;
+
+import frc.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.*;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class HatchIntake {
+    
+    private static HatchIntake instance = null;
+
+    private HatchIntake() {
+
+    }
+    
+    
+
+    public static HatchIntake getInstance() {
+        if (instance == null) {
+            instance = new HatchIntake();
+        }
+        return instance;
+    }
+}
