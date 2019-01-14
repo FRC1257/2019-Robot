e84a99ba2e3c0b7295126cc03342e57b3a79bfe7
Microwaved-Icecream <adu1@ucvts.org>
2019-01-14 17:54:13 -0500

    Feedfoward shoud be this

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -2,8 +2,8 @@ package frc.robot;
 
 public class RobotMap {
 
-    public static final double PID_LEFT[] = {0.1, 1.0, 0, 0}; // Placeholder: F, P, I, D
-    public static final double PID_RIGHT[] = {0.1, 1.0, 0, 0}; // Placeholder: F, P, I, D
+    public static final double PID_LEFT[] = {1.0, 1.0, 0, 0}; // Placeholder: F, P, I, D
+    public static final double PID_RIGHT[] = {1.0, 1.0, 0, 0}; // Placeholder: F, P, I, D
     public static final int NEO_CONSTS[] = {105, 1, 5500, 10}; // for NEO: stall current, running current, RPM limit, limitRPM
     public static final int NEO_RPMLIMIT = 5500;
 
