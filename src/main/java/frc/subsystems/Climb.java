662b4b2e2a3c7490d25633293ad60abd96a6c48d
Weihong Jin <azhang1@ucvts.org>
2019-01-17 16:58:23 -0500

    Made it so there are only 2 solenoids

--- src/main/java/frc/subsystems/Climb.java
+++ src/main/java/frc/subsystems/Climb.java
@@ -3,26 +3,31 @@ package frc.subsystems;
 import frc.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.drive.*;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Climb {
     
     private static Climb instance = null;
-    DoubleSolenoid frontLeft;
-    DoubleSolenoid frontRight;
-    DoubleSolenoid backLeft;
-    DoubleSolenoid backRight;
+
+    DoubleSolenoid front;
+    DoubleSolenoid back;
+
+    VictorSPX motorF;
+    VictorSPX motorB;
+
     public boolean FrontOn;
     public boolean BackOn;
 
     public Climb() {
-        frontLeft = new DoubleSolenoid(1, 2);
-        frontRight = new DoubleSolenoid(3, 4);
-        backLeft = new DoubleSolenoid(5, 6);
-        backRight = new DoubleSolenoid(7, 8);
+        front = new DoubleSolenoid(1, 2);
+        back = new DoubleSolenoid(3, 4);
+        motorF = new VictorSPX(5);
+        motorB = new VictorSPX(6);
         FrontOn = false;
         BackOn = false;
     }
@@ -35,35 +40,31 @@ public class Climb {
     }
 
     public void frontForward() {
-        frontLeft.set(DoubleSolenoid.Value.kForward);
-        frontRight.set(DoubleSolenoid.Value.kForward);
+        front.set(DoubleSolenoid.Value.kForward);
         this.FrontOn=true;
     }
 
     public void frontReverse() {
-        frontLeft.set(DoubleSolenoid.Value.kReverse);
-        frontRight.set(DoubleSolenoid.Value.kReverse);
+        front.set(DoubleSolenoid.Value.kReverse);
         this.FrontOn=false; 
     }
 
     public void backForward() {
-        backLeft.set(DoubleSolenoid.Value.kForward);
-        backRight.set(DoubleSolenoid.Value.kForward);
+        back.set(DoubleSolenoid.Value.kForward);
         this.BackOn = true;
     }
 
     public void backReverse() {
-        backLeft.set(DoubleSolenoid.Value.kReverse);
-        backRight.set(DoubleSolenoid.Value.kReverse);
+        back.set(DoubleSolenoid.Value.kReverse);
         this.BackOn = false;
     }
 
     public void reset() {
-        frontLeft.set(DoubleSolenoid.Value.kReverse);
-        frontRight.set(DoubleSolenoid.Value.kReverse);
-        backLeft.set(DoubleSolenoid.Value.kReverse);
-        backRight.set(DoubleSolenoid.Value.kReverse);
+        front.set(DoubleSolenoid.Value.kReverse);
+        back.set(DoubleSolenoid.Value.kReverse);
         FrontOn = false;
         BackOn = false;
     }
+    
+
 }
