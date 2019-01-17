6b77cb56d1f5af8e83a6eb2ebd1ef8e65d4515e2
ArthurChen1 <AChen1@ucvts.org>
2019-01-17 17:05:54 -0500

    Added conversion to inches for encoders

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -8,7 +8,8 @@ public class RobotMap {
     public static final int NEO_CONSTS[] = {105, 1, 5500, 10}; // for NEO: stall current (Amps), running current (Amps), RPM limit, limitRPM
     public static final int NEO_RPMLIMIT = 5500; // RPM
     public static final double P_WASHOUT = 1.0; // Placeholder: P control for washout
-
+    public static final double diameter = 6.0;
+    public static final double pi = 3.14159265359;
     // Channel IDs
     public static final int MOTORS[] = {0, 1, 2, 3}; // In this order: FL, FR, BL, BR
 }
