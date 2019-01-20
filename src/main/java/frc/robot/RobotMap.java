44dbaf42948c7f4a2a8144b0305536e55db1c143
ben-ji-git-hub <45372648+ben-ji-git-hub@users.noreply.github.com>
2019-01-20 14:17:13 -0500

    Volts>>cm conversion factor + comments

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -9,23 +9,27 @@ public class RobotMap {
      */
     
      //Cargo Intake
-     public static double CARGO_OUTTAKE_SPEED = 1.0;
+     
      //Maximum possible speed for cargo ejection as a percentage of total motor capacity
-     public static double CARGO_INTAKE_SPEED = -1.0;
+     public static double CARGO_OUTTAKE_SPEED = 1.0;
      //Maximum possible speed for cargo intake as a percentage of total motor capacity
-     public static final int CARGO_INTAKE_PORT = 1;
+     public static double CARGO_INTAKE_SPEED = -1.0;
      //Port of Intake Motor
-     public static final int OPERATOR_CONTROLLER = 1;
+     public static final int CARGO_INTAKE_PORT = 1;
      //Xbox Controller Port
-
+     public static final int OPERATOR_CONTROLLER = 1;
 
      //Infared Sensor
      //https://home.roboticlab.eu/en/examples/sensor/ir_distance
-     public static final int CARGO_INFARED_PORT = 1;
+     
      //Port of Infrared Sensor
-     public static double CARGO_PONR = 30.0;
+     public static final int CARGO_INFARED_PORT = 1;
+     //Factor for converting analogue voltage to centimeter value
+     public static final CARGO_INFRARED_CONVERSION_FACTOR = (2.6/-70);
      //Point of no return at which the cargo needs to be pulled back; defines upper threshold for P-loop
-     public static double CARGO_SENSOR_LOWER_THRESHOLD = 10.0;
+     public static double CARGO_PONR = 30.0;
      //Furtherst distance at which cargo is too close to be accurately measured; defines lower threshold for P-loop
+     public static double CARGO_SENSOR_LOWER_THRESHOLD = 10.0;
+     //kP = ratio of maximum intake speed to distance upper bound (PONR)
      public static double kP = CARGO_INTAKE_SPEED/CARGO_PONR;
 }
