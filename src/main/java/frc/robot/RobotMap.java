7054eae67f81a87859669a6517e8fa988c44e212
ben-ji-git-hub <45372648+ben-ji-git-hub@users.noreply.github.com>
2019-01-20 15:03:32 -0500

    Beautification

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -8,18 +8,17 @@ public class RobotMap {
      * The units of each measurement should be specified in a comment
      */
     
-     //Cargo Intake
+     //CARGO INTAKE MOTOR
      
+     //Port of Intake Motor
+     public static final int CARGO_INTAKE_PORT = 1;
      //Maximum possible speed for cargo ejection as a percentage of total motor capacity
      public static double CARGO_OUTTAKE_SPEED = 1.0;
      //Maximum possible speed for cargo intake as a percentage of total motor capacity
      public static double CARGO_INTAKE_SPEED = -1.0;
-     //Port of Intake Motor
-     public static final int CARGO_INTAKE_PORT = 1;
-     //Xbox Controller Port
-     public static final int OPERATOR_CONTROLLER = 1;
-
-     //Infared Sensor
+        //ideal speed twice that of robot drive speed (12ft/s * 2 = 24 ft/s)
+    
+     //CARGO INFRARED SENSOR
      //https://home.roboticlab.eu/en/examples/sensor/ir_distance
      
      //Port of Infrared Sensor
@@ -32,4 +31,10 @@ public class RobotMap {
      public static double CARGO_SENSOR_LOWER_THRESHOLD = 10.0;
      //kP = ratio of maximum intake speed to distance upper bound (PONR)
      public static double kP = CARGO_INTAKE_SPEED/CARGO_PONR;
+    
+    
+     //MISCELLANEOUS
+    
+     //Xbox Controller Port
+     public static final int OPERATOR_CONTROLLER = 1;
 }
